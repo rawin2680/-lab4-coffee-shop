39830b14814069aa6d0501151f806a8ca7623ebb
- <->
2025-12-25 15:51:04 +0700

    Final update for Lab 4

---
+++ -lab4-coffee-shop-master/server/src/app.js
@@ -0,0 +1,18 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+ 
+const app = express()
+ 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+ 
+// เรียกใช้ routes.js โดยส่ง app เข้าไป
+require('./routes')(app)
+ 
+// ไม่ต้องมี route อื่นแล้วตาม Lab5
+ 
+let port = 8081
+ 
+app.listen(port, function () {
+    console.log('server running on ' + port)
+})
